00bd9deae434d5f742ba7e4a2b59020e21b7dded
Vincent Neparidze <nva@akmetron.ru>
2018-11-26 14:05:02 +0300

    https://docs.microsoft.com/ru-ru/aspnet/core/tutorials/razor-pages-vsc/sql?view=aspnetcore-2.1

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -31,6 +28,8 @@ namespace RazorPagesMovie
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }

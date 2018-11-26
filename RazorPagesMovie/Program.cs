b6b6559f800fd163e5b62d5bdcd3b74bcf4ae9f0
Vincent Neparidze <nva@akmetron.ru>
2018-11-26 15:47:42 +0300

    https://docs.microsoft.com/ru-ru/aspnet/core/tutorials/razor-pages-vsc/da1?view=aspnetcore-2.1

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.ExpressionVisitors.Internal;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -14,11 +18,36 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+//            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = BuildWebHost(args);
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                try
+                {
+                    MovieContext context = services.GetRequiredService<MovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception exception)
+                {
+                    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exception, "An error occurred seeding the DB.");
+                }
+            }
+            
+            host.Run();
         }
+
+        private static IWebHost BuildWebHost(string[] args)
+            => WebHost.CreateDefaultBuilder(args)
+                      .UseStartup<Startup>()
+                      .Build();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .UseStartup<Startup>();
     }
 }

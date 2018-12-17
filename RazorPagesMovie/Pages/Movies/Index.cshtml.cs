42b251562587b7dcc1cab6fbee721104ed745008
Vincent Neparidze <nva@akmetron.ru>
2018-12-17 16:57:15 +0300

    https://docs.microsoft.com/ru-ru/aspnet/core/tutorials/razor-pages/da1?view=aspnetcore-2.2

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -19,10 +20,20 @@ namespace RazorPagesMovie.Pages_Movies
         }
 
         public IList<Movie> Movie { get; set; }
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<Movie> movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+                movies = movies.Where(s => s.Title.Contains(searchString));
+
+            Movie = await movies.ToListAsync();
+            SearchString = searchString;
         }
     }
 }

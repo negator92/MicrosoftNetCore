0a3c692159769dea95f351d52f4bfe73a5c4032a
Vincent Neparidze <nva@akmetron.ru>
2018-12-30 09:40:12 +0300

    next article https://docs.microsoft.com/ru-ru/aspnet/core/tutorials/razor-pages/new-field?view=aspnetcore-2.2&tabs=visual-studio-code

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -20,20 +20,29 @@ namespace RazorPagesMovie.Pages_Movies
         }
 
         public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync(string searchString)
+        public async Task OnGetAsync()
         {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             IQueryable<Movie> movies = from m in _context.Movie
                          select m;
 
-            if (!string.IsNullOrEmpty(searchString))
-                movies = movies.Where(s => s.Title.Contains(searchString));
+            if (!string.IsNullOrEmpty(SearchString))
+                movies = movies.Where(s => s.Title.Contains(SearchString));
 
+            if (!string.IsNullOrEmpty(MovieGenre))
+                movies = movies.Where(x => x.Genre == MovieGenre);
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
-            SearchString = searchString;
         }
     }
 }

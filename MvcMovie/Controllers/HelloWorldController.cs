efbd1cd973725f58922a3fd68468bf9abaa34c69
Vincent Neparidze <negator92@gmail.com>
2019-01-10 21:10:59 +0300

    https://docs.microsoft.com/ru-ru/aspnet/core/tutorials/first-mvc-app/adding-view?view=aspnetcore-2.2&tabs=visual-studio-code

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,11 +1,12 @@
 using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public string Index()
-            => "This is my default action";
+        public IActionResult Index()
+            => View();
 
         public string Welcome(string name, int numTimes = 1)
             => HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
